3bb0231cbe9a2e71af7e1313ef400ed58dccb595
Will King <contact@wking.dev>
2024-11-27 09:11:26 -0600

    Fixing error when downloading private pdf on latest

--- app/routes/documentation.private.$product.$ref[.]pdf.ts
+++ app/routes/documentation.private.$product.$ref[.]pdf.ts
@@ -1,19 +1,32 @@
-import type { LoaderFunctionArgs } from '@remix-run/node'
+import { redirect, type LoaderFunctionArgs } from '@remix-run/node'
 import fs from 'fs/promises'
 import path from 'path'
 import invariant from 'tiny-invariant'
 import { privateContentPath } from '~/lib/docs/fs.server.ts'
+import { validateParams } from '~/lib/docs/params.server.ts'
+import { getProductVersions, getVersion } from '~/lib/docs/versions.server.ts'
 import { pdf } from '~/utils/responses.server.ts'
 
 export { headers } from '~/components/layout/Content.tsx'
 
 export async function loader({ params }: LoaderFunctionArgs) {
-	let { product, ref } = params
+	let { product, ref, '*': splat } = params
 	invariant(product, 'expected `params.product`')
 	invariant(ref, 'expected `params.ref`')
 
+	const versions = await getProductVersions({ product, isPrivate: true })
+	const { version } = getVersion(versions, ref)
+
+	let betterUrl = validateParams(versions, ['latest'], {
+		product,
+		ref,
+		'*': splat,
+	})
+
+	if (betterUrl) throw redirect('/' + betterUrl)
+
 	const pdfResponse = await fs.readFile(
-		path.join(privateContentPath(product, ref), 'documentation.pdf'),
+		path.join(privateContentPath(product, version), 'documentation.pdf'),
 	)
 	return pdf(pdfResponse)
 
