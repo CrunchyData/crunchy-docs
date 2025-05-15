fadba1b41a41257d6053f80233e1f6715cb8dd39
Will King <contact@wking.dev>
2025-05-15 15:55:33 -0500

    Updating the pdf generation

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -1,12 +1,20 @@
-import { savePDF } from "~/lib/docs/pdf.server.tsx";
-import { getProductAccess } from "~/lib/docs/utils.ts";
-import { getProductVersions, getVersion } from "~/lib/docs/versions.server.ts";
+import { savePDF } from '~/lib/docs/pdf.server.tsx'
+import { getProductAccess } from '~/lib/docs/utils.ts'
+import { getProductVersions, getVersion } from '~/lib/docs/versions.server.ts'
 
-const args = process.argv.slice(2)
-const [product, ref] = args
-const access = getProductAccess(product)
+//const args = process.argv.slice(2)
+//const [product, ref] = args
+//
+const products = ['postgres-operator', 'postgres-operator-private']
+const refs = ['5.3', '5.4', '5.5', '5.6', '5.7', '5.8']
 
-const versions = await getProductVersions({ product })
-const { version } = getVersion(versions, ref)
+async function generate(p: string, r: string) {
+	const access = getProductAccess(p)
 
-savePDF({ product, ref, version, access })
+	const versions = await getProductVersions({ product: p })
+	const { version } = getVersion(versions, r)
+
+	savePDF({ product: p, ref: r, version, access })
+}
+
+products.forEach(p => refs.forEach(r => generate(p, r)))
